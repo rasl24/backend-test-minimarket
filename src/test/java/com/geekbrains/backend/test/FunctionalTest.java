b10bf60a74699a75675f202877d2204afeac5803
rs <rasl_24@mail.ru>
2021-12-29 18:51:30 +0300

    Jenkins

--- src/test/java/com/geekbrains/backend/test/FunctionalTest.java
+++ src/test/java/com/geekbrains/backend/test/FunctionalTest.java
@@ -2,9 +2,11 @@ package com.geekbrains.backend.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public abstract class FunctionalTest {
@@ -12,7 +14,7 @@ public abstract class FunctionalTest {
     // метод считывания токена
     public static Properties readProperties() throws Exception {
         Properties properties = new Properties();
-        Files.readAllLines(Path.of(
+        Files.readAllLines(Paths.get(
                 FunctionalTest.class.getResource("test.properties").toURI()))
             .forEach(str -> {
                 String[] props = str.split("=");
@@ -24,9 +26,13 @@ public abstract class FunctionalTest {
     // метод получения данных из файла
     public String getStringResource(String name) throws IOException {
         String dir = getClass().getSimpleName();
-        byte[] bytes = getClass().getResourceAsStream(dir + "/" + name)
-                .readAllBytes();
-        return new String(bytes, StandardCharsets.UTF_8);
+        InputStream is = getClass().getResource(dir + "/" + name)
+                .openStream();
+        StringBuilder s = new StringBuilder();
+        while (is.available() > 0){
+            s.append((char) is.read());
+        }
+        return s.toString();
     }
 
     // метод получения файла
